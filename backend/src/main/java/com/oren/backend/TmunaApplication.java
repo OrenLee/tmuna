074bf0bcdaf9d13c6146e651a45d59a687289757
Oren <orenlee8@gmail.com>
2021-09-04 14:45:47 +0300

    init2

--- backend/src/main/java/com/oren/backend/TmunaApplication.java
+++ backend/src/main/java/com/oren/backend/TmunaApplication.java
@@ -1,13 +1,14 @@
-package com.example.backend;
+package com.oren.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class TmunaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(TmunaApplication.class, args);
+		System.out.println("WORKING");
 	}
 
 }
